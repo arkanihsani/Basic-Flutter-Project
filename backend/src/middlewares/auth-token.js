741795f64ab6f45d82d2611df503f992a7555714
arkanihsani <arkansekolah123@gmail.com>
2025-09-01 11:51:11 +0700

    fix: formatting middleware files

--- backend/src/middlewares/auth-token.js
+++ backend/src/middlewares/auth-token.js
@@ -1,30 +1,29 @@
-import jwt from "jsonwebtoken";
-import BaseError from "../errors/base-error.js";
-import db from "../utils/db.js";
+import jwt from 'jsonwebtoken'
+import BaseError from '../errors/base-error.js'
+import db from '../utils/db.js'
 
 const authToken = async (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
-    const token = authHeader.split(" ")[1];
+    const authHeader = req.headers.authorization
+    const token = authHeader.split(' ')[1]
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
     const user = await db.user.findUnique({
       where: { id: decoded.id },
-    });
+    })
 
     if (!user) {
-      return next(BaseError.forbidden("User Not Found"));
+      return next(BaseError.forbidden('User Not Found'))
     }
 
-    req.user = user;
-    next();
+    req.user = user
+    next()
   } catch (err) {
-    if (err.message === "jwt expired") {
-      return next(BaseError.forbidden("Token Expired"));
-    } else {
-      return next(BaseError.forbidden("Token Is Invalid"));
+    if (err.message === 'jwt expired') {
+      return next(BaseError.forbidden('Token Expired'))
     }
+    return next(BaseError.forbidden('Token Is Invalid'))
   }
-};
+}
 
-export default authToken;
+export default authToken
--- backend/src/middlewares/request-validator.js
+++ backend/src/middlewares/request-validator.js
@@ -1,31 +1,28 @@
-const validate = (schema, source = "body") => (req, res, next) => {
-  let dataToValidate;
-  
+const validate = (schema, source = 'body') => (req, res, next) => {
+  let dataToValidate
+
   switch (source) {
-    case "params":
-      dataToValidate = req.params;
-      break;
-    case "query":
-      dataToValidate = req.query;
-      break;
-    case "body":
+    case 'params':
+      dataToValidate = req.params
+      break
+    case 'query':
+      dataToValidate = req.query
+      break
+    case 'body':
     default:
-      dataToValidate = req.body;
-      break;
+      dataToValidate = req.body
+      break
   }
-  
-  const { error } = schema.validate(dataToValidate, { abortEarly: false });
+
+  const { error } = schema.validate(dataToValidate, { abortEarly: false })
   if (error) {
     const messages = error.details
-      .map((d) => {
-        let message = d.message;
-        message = message.replace(/"([^"]+)"/g, '$1');
-        return message;
-      })
-      .join(", ");
-    return res.status(400).json({ success: false, message: messages });
+      .map((d) => d.message.replace(/"([^"]+)"/g, '$1'))
+      .join(', ')
+    return res.status(400).json({ success: false, message: messages })
   }
-  next();
-};
 
-export default validate;
+  next()
+}
+
+export default validate
